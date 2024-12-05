502ca059badb4ff1b383cc31d4db3c8594833657
archisaa <archisa.dash.cse.2023@nist.edu>
2024-12-05 11:46:32 +0530

    Add files via upload

---
+++ AboutUs.js
@@ -0,0 +1,24 @@
+// linking home page 
+function openHome(){
+    window.location.href="home.html";
+}
+
+// linking about us page 
+function openAbout(){
+    window.location.href="AboutUs.html";
+}
+
+// linking contact us page 
+function openContact(){
+    window.location.href="ContactUs.html";
+}
+
+//linking log in page 
+function openLogIn(){
+    window.location.href="SignIn.html";
+}
+
+//linking register page
+function openRegister(){
+    window.location.href=""
+}
